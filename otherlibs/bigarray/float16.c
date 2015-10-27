21122a2fae8c5f6dd8e1f8b473aa02495dbd5b58
Anton Yabchinskiy <arn@bestmx.ru>
2015-10-27 18:20:13 +0300

    Import C sources for float16 elements

---
+++ otherlibs/bigarray/float16.c
@@ -0,0 +1,21 @@
+#include "float16.h"
+
+void
+float16_init(void)
+{
+    /* TODO */
+}
+
+caml_ba_uint16
+float16_of_float32(float)
+{
+    /* TODO */
+    return 0;
+}
+
+float
+float16_to_float32(caml_ba_uint16)
+{
+    /* TODO */
+    return 0.0f;
+}
---
+++ otherlibs/bigarray/float16.h
@@ -0,0 +1,10 @@
+#include "bigarray.h"
+
+void
+float16_init(void);
+
+caml_ba_uint16
+float16_of_float32(float);
+
+float
+float16_to_float32(caml_ba_uint16);
